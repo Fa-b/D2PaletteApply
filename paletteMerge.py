0477b4b716e122630730a21e2cfe35a7acd20b60
Fa-b <41373206+Fa-b@users.noreply.github.com>
2020-03-11 15:08:10 +0100

    not working yet

--- paletteMerge.py
+++ paletteMerge.py
@@ -1,6 +1,8 @@
 import sys
+
 import os
 import struct
+import PIL.Image
 
 if sys.version_info[0] > 2:
     from tkinter import *
@@ -100,33 +102,73 @@ for map in shift:
     newPalettes.append(palette)
     num += 1
     
-if sys.version_info[0] > 2:
-    spriteName = filedialog.askopenfilename(initialdir = "./assets",title = "Select D2 Sprite (won't be modified)",filetypes = (("d2 sprite files","*.dc6"),("all files","*.*")))
-else:
-    spriteName = tkFileDialog.askopenfilename(initialdir = "./assets",title = "Select D2 Sprite (won't be modified)",filetypes = (("d2 sprite files","*.dc6"),("all files","*.*")))
+    if sys.version_info[0] > 2:
+        spriteName = filedialog.askopenfilename(initialdir = "./assets",title = "Select D2 Sprite (won't be modified)",filetypes = (("d2 sprite files","*.dc6"),("all files","*.*")))
+    else:
+        spriteName = tkFileDialog.askopenfilename(initialdir = "./assets",title = "Select D2 Sprite (won't be modified)",filetypes = (("d2 sprite files","*.dc6"),("all files","*.*")))
 
-fileHeader = []
-framePointers = []
-frameHeader = []
-sprite = []
-with open(spriteName, "rb") as f:
-    long = struct.unpack('<i', f.read(4))[0]
-    while len(fileHeader) < 6:
-        fileHeader.append(long)
-        long = struct.unpack('<i', f.read(4))[0]
-    while len(framePointers) < (fileHeader[4] * fileHeader[5]):
-        framePointers.append(long)
-        long = struct.unpack('<i', f.read(4))[0]
-    if(len(framePointers) > 1):
-        raise ValueError('Not supporting multiple frames & directions yet :-(.')
-    # TODO read file into bytearray and work with that, that way we can jump to the adress offset of each frame_header
-    while len(frameHeader) < 8:
-        frameHeader.append(long)
-        long = struct.unpack('<i', f.read(4))[0]
+    fileHeader = []
+    framePointers = []
+    frameHeader = []
+    sprite = []
+    with open(spriteName, "rb") as f:
+        long = f.read(4)
+        while len(fileHeader) < 6:
+            fileHeader.append(struct.unpack('<i', long)[0])
+            long = f.read(4)
+        while len(framePointers) < (fileHeader[4] * fileHeader[5]):
+            framePointers.append(struct.unpack('<i', long)[0])
+            long = f.read(4)
+        if(len(framePointers) > 1):
+            raise ValueError('Not supporting multiple frames & directions yet :-(.')
+        # TODO read file into bytearray and work with that, that way we can jump to the adress offset of each frame_header
+        while len(frameHeader) < 7:
+            frameHeader.append(struct.unpack('<i', long)[0])
+            long = f.read(4)
+        frameHeader.append(struct.unpack('<i', long)[0])
+        data = f.read(1)
+        while data:
+            sprite.append(ord(struct.unpack('c', data)[0]))
+            data = f.read(1)
+            
+        sprite.reverse()
         
-print("File Header:", fileHeader)
-print("Frame Header:", frameHeader)
+    print("File Header:", fileHeader)
+    print("Frame Header:", frameHeader)
+    print("Sprite:", sprite)
+    sys.stdout.flush()
+    
+    print(len(sprite))
+        
+    pic = []
+    x = 0
+    y = 0
+    pic.append([])
+    for idx in range(frameHeader[7]):
+        if idx == 0x80:
+            pic[y].append(255)
+            x = 0
+            y += 1
+            pic.append([])
+            break;
+        elif idx & 0x80 == 0x80:
+            for j in range(idx & 0x80):
+                pic[y].append(255)
+        else:
+            pic[y].append(sprite[idx])
+        x += 1
+        
+    img = PIL.Image.new('RGBA', (frameHeader[1], frameHeader[2]))
+        
+    for x in range(len(pic[0])):
+        for y in range(len(pic)):
+            if pic[x][y] == 255:    # <- bug
+                img.putpixel((x,y), (0,0,0,0))
+            else:
+                img.putpixel((x,y), (palette[pic[x][y]][0],palette[pic[x][y]][1],palette[pic[x][y]][2],255))
+                
+    img.save(resDir + "/" + paletteName[0:paletteName.rfind(".")].split("/")[-1] + "_" + str(num) + ".png")
+    
 
-sys.stdout.flush()
 
 root.mainloop()
